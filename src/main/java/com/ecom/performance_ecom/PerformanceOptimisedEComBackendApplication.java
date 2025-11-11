8dfffc01f3a5d235df116316a185e562f3631f84
Ayush Baghel <ayush.devhub@gmail.com>
2025-11-11 10:52:23 +0530

    feat(app): add main Spring Boot application entry point

---
+++ src/main/java/com/ecom/performance_ecom/PerformanceOptimisedEComBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.performance_ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceOptimisedEComBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceOptimisedEComBackendApplication.class, args);
+	}
+
+}
