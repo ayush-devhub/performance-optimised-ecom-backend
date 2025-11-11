90c36370ef019c6396f6bc71d377ff1485bded17
Ayush Baghel <ayush.devhub@gmail.com>
2025-11-11 10:45:12 +0530

    chore: add initial Spring Boot project structure with Maven wrapper

---
+++ src/test/java/com/ecom/performance_ecom/PerformanceOptimisedEComBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.performance_ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceOptimisedEComBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
