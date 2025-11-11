2747d453fd7d18bb0742a39354f5805c16df4247
Ayush Baghel <ayush.devhub@gmail.com>
2025-11-11 10:50:40 +0530

    feat: add ProductService class to handle Product business logic

---
+++ src/main/java/com/ecom/performance_ecom/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.ecom.performance_ecom.service;
+
+import com.ecom.performance_ecom.model.Product;
+import com.ecom.performance_ecom.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
